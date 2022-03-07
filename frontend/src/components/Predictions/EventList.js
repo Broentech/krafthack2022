dfd4462a3a3f27ae4df620791807eda627555e5e
broentech <stian@broentech.no>
2022-03-08 00:08:22 +0100

    forgot to add eventlist

---
+++ frontend/src/components/Predictions/EventList.js
@@ -0,0 +1,27 @@
+
+import classes from './EventList.module.css';
+import {useSelector} from "react-redux";
+import PerfectScrollbar from 'react-perfect-scrollbar';
+
+const EventList = () => {
+
+    const findings = useSelector((state) => state.pred.thresholdFindings) ;
+
+    return (
+      <PerfectScrollbar className={classes.eventlist}>
+          <h1>Events</h1>
+          <ul>
+              {findings.map((finding => (
+                  <li>
+                      <div>
+                          <h5>Sensor : {finding.sensor}</h5>
+                          <p>Value : {finding.value} Time : {finding.timestamp}</p>
+                      </div>
+                  </li>
+              )))}
+          </ul>
+      </PerfectScrollbar>
+    );
+};
+
+export default EventList;
